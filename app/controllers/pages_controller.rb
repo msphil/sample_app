85b87c2a2a32d30cf80c2a6b3ca138860f120b8b
Mike Phillips <msphil@gmail.com>
2012-05-09 11:34:57 -0700

    Add micropost search to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,11 @@ class PagesController < ApplicationController
     @title = "Home"
     if signed_in?
       @micropost = Micropost.new
-      @feed_items = current_user.feed.paginate(:page => params[:page])
+      if params[:search] and params[:search].length > 0
+        @feed_items = current_user.feed.search(params[:search], :match_mode => :any).paginate(:page => params[:page])
+      else
+        @feed_items = current_user.feed.paginate(:page => params[:page])
+      end
     end
   end
 
